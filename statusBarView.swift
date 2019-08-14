87d787d05f8f99ee533badf761437cc19313f17e
pateldevang <devangpatel92@gmail.com>
2019-08-14 23:47:32 +0530

    statusBarView class added.

---
+++ statusBarView.swift
@@ -0,0 +1,19 @@
+//
+//  statusBarView.swift
+//  Pods-SVStatusBarNotification_Example
+//
+//  Created by Devang Patel on 14/08/19.
+//
+
+import Foundation
+import UIKit
+
+public class statusBarView {
+    
+}
+
+func setupReachability(_ hostName: String?) {
+    
+   
+    
+}
